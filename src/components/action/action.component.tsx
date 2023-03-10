f093a4a6d3b5afa721565189d5aea7410b767094
Adam Boucek <adamm01b@gmail.com>
2023-03-10 10:02:11 -0800

    POR-79: Feat: Add type props to action component

--- src/components/action/action.component.tsx
+++ src/components/action/action.component.tsx
@@ -2,6 +2,7 @@
 import * as React from 'react';
 type Props = {
   children: React.ReactNode;
+  type: 'link' | 'button';
 };
 export const Action = ({ children, ...props }: Props) => {
   return <div>{children}</div>;
