d5e9b3e135b248136b11921d212afa46a4b464e0
Redjinator <rmcpherson@northislandcollege.ca>
2023-03-10 02:33:10 -0800

    add:draft input component

--- src/components/input/input.component.tsx
+++ src/components/input/input.component.tsx
@@ -1,8 +1,48 @@
-// @flow
+import './input.css';
+
 import * as React from 'react';
+
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
   label: string;
 }
+
 export const Input = ({ label, ...props }: Props) => {
-  return <div></div>;
+  const [isFocused, setIsFocused] = React.useState(false);
+  const [hasError, setHasError] = React.useState(false);
+
+  // Input handlers
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value.length < 6) {
+      setHasError(true);
+    } else {
+      setHasError(false);
+    }
+    props.onChange && props.onChange(event);
+  };
+
+  return (
+    <div>
+      <label>{label}</label>
+      <input
+        {...props}
+        onChange={handleInputChange}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        className={`${isFocused ? 'focused' : ''} ${hasError ? 'error' : ''}`}
+      />
+      {hasError && (
+        <div style={{ color: 'red', textAlign: 'left' }}>
+          Error Test: Please enter at least 6 characters
+        </div>
+      )}
+    </div>
+  );
 };
