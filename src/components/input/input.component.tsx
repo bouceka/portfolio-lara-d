fa1e319b3e1f478303dff59287bd36e52c935730
bouceka <adamm01b@gmail.com>
2023-03-13 12:25:31 -0700

    feature/POR-78: Refactor: Refact input component

--- src/components/input/input.component.tsx
+++ src/components/input/input.component.tsx
@@ -1,5 +1,6 @@
-import './input.css';
+import './input.scss';
 
+import { useState } from 'react';
 import * as React from 'react';
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
@@ -10,36 +11,15 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
 }
 
 export const Input = ({ label, hint, optionalText, error, ...props }: Props) => {
-  const [isFocused, setIsFocused] = React.useState(false);
-
-  // Input handlers
-  const handleFocus = () => {
-    setIsFocused(true);
-  };
-
-  const handleBlur = () => {
-    setIsFocused(false);
-  };
-
-
   return (
     <div>
       <div className="label-container">
         <label>{label}</label>
         {optionalText ? <div className={'optional-text'}>{optionalText}</div> : null}
       </div>
-
-      <input
-        {...props}
-        onFocus={handleFocus}
-        onBlur={handleBlur}
-        className={`${isFocused ? 'focused' : ''} ${error ? 'error' : ''}`}
-      />
-
+      <input {...props} className={`${error ? 'error' : ''}`} />
       {hint ? <div className={'hint-text'}>{hint}</div> : null}
-
       {error ? <div className={'error-text'}>{error}</div> : null}
-
     </div>
   );
 };
