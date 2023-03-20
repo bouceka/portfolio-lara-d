61d25aca9620c3cde39892bf201afbf6711372b6
Reginald McPherson <71242881+Redjinator@users.noreply.github.com>
2023-03-20 13:24:49 -0700

    feat: base image for services cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import './styles/App.scss';
+import FashiomImg from './assets/images/CB-LaraDrummond-Moodboard-25.jpg'
 
 import { MyForm } from './components/form/form.component';
 
 function App() {
-  return <div className="App">Test</div>;
+  return (
+    <div className="App">
+      <div className='container'>
+        <img src={FashiomImg}  alt='img'/>
+        <div className='serviceLabel'>
+          
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
