dea8636cc14530eb618902683dd215c4c0a00078
Reginald McPherson <71242881+Redjinator@users.noreply.github.com>
2023-03-20 12:58:33 -0700

    POR-91: Chore: Test

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,7 @@ import './styles/App.scss';
 import { MyForm } from './components/form/form.component';
 
 function App() {
-  return (
-    <div className="App">
-      <MyForm />
-    </div>
-  );
+  return <div className="App">Test</div>;
 }
 
 export default App;
