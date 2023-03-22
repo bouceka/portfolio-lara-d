c2c4c65e503c41ac87fe9169aaf38e3a0afb672c
bouceka <adamm01b@gmail.com>
2023-03-22 14:38:25 -0700

    POR-91: Chore refactor service cards

--- src/pages/about-me.tsx
+++ src/pages/about-me.tsx
@@ -3,23 +3,7 @@ import '../styles/pages/about-me.styles.scss';
 
 import * as React from 'react';
 
-import EditorialImage from '../assets/images/CB-LaraDrummond-Moodboard-25.jpg';
-import FashionImage from '../assets/images/CB-LD-Fashion-1-8.jpg';
-import FoodImage from '../assets/images/CB-LD-Food4.jpg';
-import ProdctImage from '../assets/images/CB-LD-Product5.jpg';
-import { ServiceCard } from '../components/servicesCard/servicesCard.component';
 
 export const AboutMe = () => {
-  return (
-    <div>
-      <div className="service-card-container">
-        <ServiceCard serviceLabel="PRODUCT" color="secondary" image={ProdctImage} />
-        <ServiceCard serviceLabel="FOOD" color="accent-1" image={FoodImage} />
-      </div>
-      <div className="service-card-container">
-        <ServiceCard serviceLabel="EDITORIAL" color="accent-1" image={EditorialImage} />
-        <ServiceCard serviceLabel="FASHION" color="primary" image={FashionImage} />
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
