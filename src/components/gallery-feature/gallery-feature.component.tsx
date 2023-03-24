3a47700a54229d70430537aa584d5f5fe6d652fa
Adam Boucek <adamm01b@gmail.com>
2023-03-23 17:24:25 -0700

    POR-101: Feat: Create gellery detail page

--- src/components/gallery-feature/gallery-feature.component.tsx
+++ src/components/gallery-feature/gallery-feature.component.tsx
@@ -1,7 +1,8 @@
 // @flow
 import * as React from 'react';
+import { ImageCarousel } from './image-carousel.component';
 interface Props {
-  imageList: { alt: string; imgURL: string };
+  imageList: { alt: string; imgURL: string }[];
   year: string;
   title: string;
 }
@@ -9,10 +10,12 @@ export const GalleryFeature = ({ title, year, imageList }: Props) => {
   return (
     <section className="gallery-feature">
       <div className="gallery-feature__text">
-        <h5 className="recent-projects-item__title">{title}</h5>
-        <span className="recent-projects-item__year">{year}</span>
+        <h5 className="gallery-feature__title">{title}</h5>
+        <h6 className="gallery-feature__year">{year}</h6>
       </div>
-      <div className="gallery-feature__image-slider"></div>
+      <div className="gallery-feature__image-slider">
+        <ImageCarousel imageList={imageList} />
+      </div>
     </section>
   );
 };
--- src/components/services-card/services-card.component.tsx
+++ src/components/services-card/services-card.component.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { ReactComponent as ArrowSVG } from '../../assets/Arrow-Right.svg';
+import { Services } from '../../types/service';
 import { Action } from '../action/action.component';
 
 // Create interface for props
@@ -8,16 +9,17 @@ interface Props {
   serviceLabel: string;
   image: string;
   color: 'primary' | 'secondary' | 'accent-1';
+  link: Services;
 }
 
-export const ServiceCard = ({ serviceLabel, color, image }: Props) => {
+export const ServiceCard = ({ serviceLabel, color, image, link }: Props) => {
   return (
     <div className="container" style={{ backgroundImage: `url(${image})` }}>
       <div className="left-bar" style={{ backgroundColor: `var(--color-${color})` }}>
         <div className="left-bar-text">{serviceLabel}</div>
       </div>
       <div className="bottom-bar">
-        <Action as="link" styleType="cta" className="secondary" to="/gallery">
+        <Action as="link" styleType="cta" className="secondary" to={link}>
           SEE MORE <ArrowSVG />
         </Action>
       </div>
