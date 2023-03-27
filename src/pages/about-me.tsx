2a9f9a89ec91360a37e4fe89fb5f4ace48f67074
Reginald McPherson <71242881+Redjinator@users.noreply.github.com>
2023-03-27 11:45:31 -0700

    POR-90:Feat:add title and bio style incomplete

--- src/pages/about-me.tsx
+++ src/pages/about-me.tsx
@@ -6,13 +6,43 @@ import { Title } from '../components/title/title.component';
 
 export const AboutMe = () => {
   return (
-    <div className="page">
-      <div className="row">
-        <Title
-          title="MEET LARA DRUMMOND"
-          subtitle="Hi, it’s nice to meet you! With 14+ years of experience in editorial, product, food, and fashion photography, I’m here to meet your photography needs."
-        />
-      </div>
+    <div className="page row">
+      <Title
+        title="MEET LARA DRUMMOND"
+        subtitle="Hi, it’s nice to meet you! With 14+ years of experience in editorial, product, food, and fashion photography, I’m here to meet your photography needs."
+      />
+      <section className="section-bio">
+        <div className="bio-container">
+          <p className="bio-text">
+            Since the start of my career, I&apos;’ve always loved a good challenge.
+            Instead of focusing on a single area of photography, I choose to specialize in
+            the 4 areas I felt most passionate about: editorial, product, food, and
+            fashion photography. My photography style is flexible to accommodate a range
+            of needs, from informational and functional, to highly creative and full of
+            personality. My passion lies in helping businesses of all sizes and industries
+            elevate their brand presence and share their stories through photography. I
+            love developing and executing a brand&apos;’s creative vision with exceptional
+            custom images, so that business owners can focus on the part they do best:
+            growing their business. Sourcing high-quality professional photos can be
+            overwhelming, so I&apos;’m here to make the photo shoot process as smooth and
+            seamless as possible. I can connect you with all the assets and collaborators
+            you need, such as stylists, makeup artists, set designers, digital marketers,
+            and more. All you need to do is meet me for an incredible shoot day, and show
+            off your incredible new photos! When booking a photography session with me, I
+            guarantee your business will receive hard-hitting visuals that hit the mark,
+            every time.
+          </p>
+
+          <p className="bio-signature">- LARA DRUMMOND</p>
+        </div>
+
+        <div className="bio-image">
+          <img
+            src="https://res.cloudinary.com/dufli3jum/image/upload/v1679542368/LaraPorfolio/Lara-Drummond/small/CB-LD-Lara2_gi8fmg.jpg"
+            alt="Lara Drummond"
+          />
+        </div>
+      </section>
     </div>
   );
 };
