c64860f463d85fd7274568976352907b9e1a762f
Reginald McPherson <71242881+Redjinator@users.noreply.github.com>
2023-03-27 10:21:36 -0700

    POR-90:Feat: add title

--- src/pages/about-me.tsx
+++ src/pages/about-me.tsx
@@ -2,6 +2,17 @@
 
 import * as React from 'react';
 
+import { Title } from '../components/title/title.component';
+
 export const AboutMe = () => {
-  return <div></div>;
+  return (
+    <div className="page">
+      <div className="row">
+        <Title
+          title="MEET LARA DRUMMOND"
+          subtitle="Hi, it’s nice to meet you! With 14+ years of experience in editorial, product, food, and fashion photography, I’m here to meet your photography needs."
+        />
+      </div>
+    </div>
+  );
 };
