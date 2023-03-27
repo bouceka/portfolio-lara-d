69f79012be3c9c3eb5f52e653bfab525ae1b984e
bouceka <adamm01b@gmail.com>
2023-03-27 09:54:43 -0700

    POR-104: Style: Change styling

--- src/components/featured-in/featured-in.component.tsx
+++ src/components/featured-in/featured-in.component.tsx
@@ -13,24 +13,26 @@ import { Title } from '../title/title.component';
 
 export const FeaturedIn = () => {
   return (
-    <section className="row">
-      <div className="featured-section">
-        <Title title="FEATURED IN:" />
-        <div className="featured-logos-container">
-          <div className="featured-logo">
-            <img src={TrioLogo} alt="Trio Magazine Logo" />
-          </div>
-          <div className="featured-logo">
-            <img src={YamLogo} alt="Yam Magazine Logo" />
-          </div>
-          <div className="featured-logo">
-            <img src={CvCollectiveLogo} alt="CV Collective Logo" />
-          </div>
-          <div className="featured-logo">
-            <img src={BoulevardLogo} alt="Boulevard Magazine Logo" />
-          </div>
-          <div className="featured-logo">
-            <img src={EdibleVancouverLogo} alt="Edible Vancouver Island Logo" />
+    <section className="featured">
+      <div className="row">
+        <div className="featured-section">
+          <Title title="FEATURED IN:" />
+          <div className="featured-logos-container">
+            <div className="featured-logo">
+              <img src={TrioLogo} alt="Trio Magazine Logo" />
+            </div>
+            <div className="featured-logo">
+              <img src={YamLogo} alt="Yam Magazine Logo" />
+            </div>
+            <div className="featured-logo">
+              <img src={CvCollectiveLogo} alt="CV Collective Logo" />
+            </div>
+            <div className="featured-logo">
+              <img src={BoulevardLogo} alt="Boulevard Magazine Logo" />
+            </div>
+            <div className="featured-logo">
+              <img src={EdibleVancouverLogo} alt="Edible Vancouver Island Logo" />
+            </div>
           </div>
         </div>
       </div>
